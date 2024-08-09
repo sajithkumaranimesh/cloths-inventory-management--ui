ce09c8523714d4c6e5023a473b9372f129d041e7
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-08-09 16:40:41 +0530

    add menu bar

---
+++ src/app/page/supplier/manage-supplier/manage-supplier.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-supplier',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-supplier.component.html',
+  styleUrl: './manage-supplier.component.css'
+})
+export class ManageSupplierComponent {
+
+}
