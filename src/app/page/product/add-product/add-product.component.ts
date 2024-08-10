466c7656d2f7cfa61c9f80cb12d3029209241dfc
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-08-10 17:58:23 +0530

    product form

--- src/app/page/product/add-product/add-product.component.ts
+++ src/app/page/product/add-product/add-product.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
-export class AddProductComponent {
+export class AddProductComponent implements OnInit{
+
+
+  currentDate: string | undefined;
+  currentTime: string | undefined;
+
+  
+  productForm = new FormGroup({
+    name: new FormControl(),
+    size: new FormControl(),
+    price: new FormControl(),
+    stockQuantity: new FormControl(),
+    description: new FormControl(),
+    isAvailable: new FormControl(),
+    createdAt: new FormControl(),
+    modifiedAt: new FormControl(),
+    category_id: new FormControl(),
+    supplier_id: new FormControl()
+  });
+
+  ngOnInit(): void {
+    this.currentDate = new Date().toLocaleDateString();
+    this.currentTime = new Date().toLocaleTimeString();
+
+    this.productForm.controls['createdAt'].setValue(this.currentDate + ' ' + this.currentTime);
+    this.productForm.controls['modifiedAt'].setValue(this.currentDate + ' ' + this.currentTime);
+  }
+
+
+  addProduct(){
+    console.log(this.productForm);
+  }
 
 }
