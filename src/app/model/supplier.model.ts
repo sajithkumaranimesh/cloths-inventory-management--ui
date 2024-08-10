48bc3baaa50ccd9cc24d4c4fbd8afa14346b6db7
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-08-10 13:40:52 +0530

    supplier form 1

---
+++ src/app/model/supplier.model.ts
@@ -0,0 +1,10 @@
+
+
+export class Supplier{
+    id?: number;
+    name: string | undefined | null;
+    email: string | undefined | null;
+    company: string | undefined | null;
+    createdAt: string | undefined | null;
+    modifiedAy: string |undefined | null;
+}
--- src/app/page/supplier/add-supplier/add-supplier.component.ts
+++ src/app/page/supplier/add-supplier/add-supplier.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-supplier',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-supplier.component.html',
   styleUrl: './add-supplier.component.css'
 })
 export class AddSupplierComponent {
 
+
+  supplierForm = new FormGroup({
+    name: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required]),
+    company: new FormControl('',[Validators.required]),
+    createdAt: new FormControl('',[Validators.required]),
+    modifiedAt: new FormControl('',[Validators.required])
+  });
+
+  addSupplier(){
+    console.log(this.supplierForm);
+  }
 }
