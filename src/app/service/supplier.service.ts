400b7b1dce77c505896dab06ddd9ed2356225cbd
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-08-11 19:01:57 +0530

    supplier service

--- src/app/service/supplier.service.ts
+++ src/app/service/supplier.service.ts
@@ -13,8 +13,20 @@ import { Injectable } from "@angular/core";
         return this.http.post('http://localhost:8080/supplier',supplierObj);
     }
 
-    // retrieve(){
-    //     return this.http
-    // }
+    retrieve(){
+        return this.http.get<any>('http://localhost:8080/supplier');
+    }
+
+    retireveById(id:any){
+        return this.http.get<any>(`http://localhost:8080/supplier${id}`)
+    }
+
+    update(supplierObj:any){
+        return this.http.put('http://localhost:8080/supplier',supplierObj);
+    }
+
+    delete(id:any){
+        return this.http.delete(`http://localhost:8080/supplie${id}r`);
+    }
  }
 
