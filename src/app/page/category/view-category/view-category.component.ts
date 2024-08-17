e4e5d21fdefc90d29d81b27456390ee380f4dad2
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-08-17 12:12:39 +0530

    editcategory done

--- src/app/page/category/view-category/view-category.component.ts
+++ src/app/page/category/view-category/view-category.component.ts
@@ -1,12 +1,13 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { CategoryService } from '../../../service/category.service';
 import { NgFor } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Injectable({providedIn: 'root'})
 @Component({
   selector: 'app-view-category',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,ReactiveFormsModule],
   templateUrl: './view-category.component.html',
   styleUrl: './view-category.component.css'
 })
@@ -22,6 +23,27 @@ export class ViewCategoryComponent implements OnInit{
     });
   }
 
+  selectedCategoryForm = new FormGroup({
+    id: new FormControl(),
+    name: new FormControl(),
+    description: new FormControl(),
+    createdAt: new FormControl(),
+    modifiedAt: new FormControl()
+  });
 
+  onEditCategory(category:any){
+    this.selectedCategoryForm.patchValue({
+      id: category.id,
+      name: category.name,
+      description: category.description,
+      createdAt: category.createdAt,
+      modifiedAt: category.modifiedAt
+    });
+  }
 
+  saveEditCategory(){
+    this.service.update(this.selectedCategoryForm.value).subscribe(response => {
+      this.ngOnInit();
+    });
+  }
 }
