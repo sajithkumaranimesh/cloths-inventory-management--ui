d23205de6d6cd926474861edf883ce9728164975
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-08-17 12:32:19 +0530

    product edit done

--- src/app/page/product/view-product/view-product.component.ts
+++ src/app/page/product/view-product/view-product.component.ts
@@ -1,12 +1,13 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { ProductService } from '../../../service/product.service';
 import { NgFor } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Injectable({providedIn:'root'})
 @Component({
   selector: 'app-view-product',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,ReactiveFormsModule],
   templateUrl: './view-product.component.html',
   styleUrl: './view-product.component.css'
 })
@@ -22,4 +23,41 @@ export class ViewProductComponent implements OnInit{
     });
   }
 
+
+  selectedProductForm = new FormGroup({
+    id: new FormControl(),
+    name: new FormControl(),
+    size: new FormControl(),
+    price: new FormControl(),
+    stockQuantity: new FormControl(),
+    description: new FormControl(),
+    isAvailable: new FormControl(),
+    createdAt: new FormControl(),
+    modifiedAt: new FormControl(),
+    category_id: new FormControl(),
+    supplier_id: new FormControl(),
+  });
+
+
+  onEditProduct(product:any){
+    this.selectedProductForm.patchValue({
+      id: product.id,
+      name: product.name,
+      size: product.size,
+      price: product.price,
+      stockQuantity: product.stockQuantity,
+      description: product.description,
+      isAvailable: product.isAvailable,
+      createdAt: product.createdAt,
+      modifiedAt: product.modifiedAt,
+      category_id: product.category_id,
+      supplier_id: product.supplier_id,
+    })
+  }
+
+  saveEditProduct(){
+    this.service.update(this.selectedProductForm.value).subscribe(response => {
+      this.ngOnInit();
+    })
+  }
 }
